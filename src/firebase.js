05a7ba4e9654e6d067566979152ae4f48b7feaf0
ayush64605 <147837963+ayush64605@users.noreply.github.com>
2025-04-02 22:15:08 +0530

    Update firebase.js
    remove credentials

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // Ensure these are correct from Firebase Console -> Project settings
 const firebaseConfig = {
-    apiKey: "AIzaSyA-NWUvF421Fgz_-h-_Sw2GoKiRGIyW4iI",
-    authDomain: "myapp-9c418.firebaseapp.com",
-    projectId: "myapp-9c418",
-    storageBucket: "myapp-9c418.appspot.com",
-    messagingSenderId: "339788127684",
-    appId: "1:339788127684:web:ce4f308c58696d0c691891",
-    measurementId: "G-6VKXV24Y66"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
